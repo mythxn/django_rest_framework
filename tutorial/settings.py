39e89272c2c23bd8db12506184ac6c444b64eb8e
mythxn <mithun.shanoj@dubizzle.com>
2021-06-23 16:59:47 +0400

    Serialization
    - create basic Snippet model
    - make corresponding Serializer
    - do the above automagically using ModelSerializer
    - basic views that send back json response

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'snippets.apps.SnippetsConfig'
+    'snippets.apps.SnippetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
