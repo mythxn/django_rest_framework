3619eaf7f96ffa3bbd71baca6a979911b4c957a5
mythxn <mithun.shanoj@dubizzle.com>
2021-06-24 13:18:41 +0400

    refactor(views.py): use routers to automagically generate urlsConf
    viewsets <3 routers

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework import permissions, renderers, viewsets
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
